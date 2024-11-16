d12d00e692c507af95aff2689acfe28691b6ad5d
renmission <missionrenjr@gmail.com>
2024-11-16 22:23:29 +0800

    Eslint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1 className='text-3xl text-green-700'>Hello World!</h1>;
+  return (
+    <h1 className="text-3xl text-green-700">
+      Hello World! adas asd asd asd add
+    </h1>
+  );
 }
